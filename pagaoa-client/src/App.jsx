a47e591ef3d20f13ebc317c54ce159164c415e5c
private-staticvoid <pagaoaachilestroy@gmail.com>
2026-03-17 12:35:41 +0800

    added new folders

--- pagaoa-client/src/App.jsx
+++ pagaoa-client/src/App.jsx
@@ -1,32 +1,10 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-
-function App() {
-  const [count, setCount] = useState(0);
-
+import React from 'react'
+const App = () => {
   return (
-    <>
-      <div className="App">
-        <header className="App Header">
-          <h1>Welcome to my React App!</h1>
-          <p>
-            Name: Achiles Troy Pagaoa <br />
-            Email: pagaoaachilestroy@gmail.com <br />
-            Other Info:
-            <a
-              href="https://github.com/private-staticvoid"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              My GitHub Account
-            </a>
-          </p>
-        </header>
-      </div>
-    </>
-  );
+    <div className='h-dvh text-7xl font-bold text-amber-600 '>App</div>
+  )
 }
-
+ 
+ 
 export default App;
+ 
