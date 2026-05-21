20ab2d76477e3315eccd5bff709496e6d9bc114f
private-staticvoid <pagaoaachilestroy@gmail.com>
2026-05-22 02:15:58 +0800

    fixing bugs

--- pagaoa-server/index.js
+++ pagaoa-server/index.js
@@ -1,6 +1,6 @@
+require("dotenv").config();
 const dns = require("node:dns");
 dns.setServers(["8.8.8.8", "1.1.1.1"]);
-require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
