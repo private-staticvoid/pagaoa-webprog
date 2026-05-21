f45e1bd60a708a8f7213780e7940e021426b3208
private-staticvoid <pagaoaachilestroy@gmail.com>
2026-05-22 00:56:55 +0800

    index fix

--- pagaoa-server/index.js
+++ pagaoa-server/index.js
@@ -53,7 +53,7 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: "Server Error" });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
-app.use(helmet());
+module.exports = app;
